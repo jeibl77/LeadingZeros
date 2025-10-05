29a7aef5af48f75fe2596c72bdcf4d4a51a59111
jeibl77 <jeibl@gmx.at>
2025-10-05 15:52:11 +0200

    add 001.001.001.001

--- LeadingZeros.cpp
+++ LeadingZeros.cpp
@@ -69,5 +69,10 @@ int main()
     RemoveLeadingZerosFromIPAddress(address);
     std::cout << address << std::endl;
 
+    strcpy_s(address, "001.001.001.001");
+    std::cout << address << " -> ";
+    RemoveLeadingZerosFromIPAddress(address);
+    std::cout << address << std::endl;
+
 }
 
